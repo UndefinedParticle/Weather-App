4ed63c404ea95682d1a60a7e2233a6b494d8f5b8
Chinmoy Mondal <5gsanmoy@gmail.com>
2024-04-05 10:15:28 +0530

    API changes : 05.04.2024

--- app/src/main/java/com/undefinedparticle/weather/BaseClass.kt
+++ app/src/main/java/com/undefinedparticle/weather/BaseClass.kt
@@ -2,7 +2,7 @@ package com.undefinedparticle.weather
 
 object BaseClass {
 
-    const val API_KEY = "88cf6dadaf8b3cd8f5f07ff8f63ac714"
+    const val API_KEY = "your_api_key"
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     const val UNITS = "metric"
 
